a17451b9ef41483a10563d5b1658da9dc4780517
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-02 13:00:06 +0300

    Additional entities.

---
+++ src/Entity/Order.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Order
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="orders")
+ * @package App\Entity
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $date;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $clientName;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $contactPhone;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $contactEmail;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $deliveryAddress;
+    /**
+     * @ORM\OneToMany(targetEntity="OrderProduct", mappedBy="product")
+     */
+    private $orderProducts;
+}
---
+++ src/Entity/OrderProduct.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Some ordered product
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="order_products")
+ * @package App\Entity
+ */
+class OrderProduct
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\ManyToOne(targetEntity="order", inversedBy="orderProducts")
+     * @ORM\JoinColumn(name="order_id", referencedColumnName="id")
+     * @var Order
+     */
+    private $order;
+    /**
+     * @ORM\OneToOne(targetEntity="Product")
+     * @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+     * @var Product
+     */
+    private $product;
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $qty;
+    /**
+     * @ORM\OneToMany(targetEntity="Addition")
+     */
+    private $selectedAdditions;
+
+    public function __construct()
+    {
+        $this->selectedAdditions = new ArrayCollection();
+    }
+}

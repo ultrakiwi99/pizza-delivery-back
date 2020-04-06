0e650a8b73cd5c66831214c0a3e2ab2dc0b9c219
Alex Kruzhkov <ultrakiwi99@gmail.com>
2020-04-06 16:21:44 +0300

    Index to admin console redirect.

--- config/bundles.php
+++ config/bundles.php
@@ -4,7 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-//    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,7 +4,19 @@
 namespace App\Controller;
 
 
-class IndexController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"}, name="app_index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return new RedirectResponse('/admin');
+    }
 }
